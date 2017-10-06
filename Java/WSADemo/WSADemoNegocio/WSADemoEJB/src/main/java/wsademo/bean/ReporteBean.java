87c1320476f25044e2772aead0c3058f5604b93a
FernandoArevaloE <fernarev@gmail.com>
2017-10-06 11:24:52 -0500

    Construcción reporte
    Se genera tabla para visualizar reporte dinámico

--- Java/WSADemo/WSADemoNegocio/WSADemoEJB/src/main/java/wsademo/bean/ReporteBean.java
+++ Java/WSADemo/WSADemoNegocio/WSADemoEJB/src/main/java/wsademo/bean/ReporteBean.java
@@ -8,12 +8,8 @@ package wsademo.bean;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -45,38 +41,7 @@ public class ReporteBean implements ReporteBeanRemote {
             reporte.getColumnas().add(metaData.getColumnName(i));
         }
         return reporte;
-//        try {
-
-//        try {
-//            Connection connection = em.unwrap(java.sql.Connection.class);
-//
-//            
-//            Connection connection = em.unwrap(java.sql.Connection.class);
-//            Statement s = connection.createStatement();
-//            ResultSet rs = s.executeQuery("select top 500 * from unico_tlf, unico_adquiriente");
-//            ResultSetMetaData metaData = rs.getMetaData();
-//            for (int i = 0; i < metaData.getColumnCount(); i++) {
-//                System.out.println(metaData.getColumnName(i));
-//            }
-//        Query query = em.createNativeQuery("select top 500 * from unico_tlf, unico_adquiriente");
-//        java.sql.ResultSetMetaData rsmd = em.unwrap(java.sql.ResultSetMetaData.class);
-//        try {
-//            for (int i = 0; i < rsmd.getColumnCount(); i++) {
-//                System.out.println(rsmd.getColumnName(i));
-//            }
-//        } catch (SQLException ex) {
-//            Logger.getLogger(ReporteBean.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        return query.getResultList();
-//        } catch (Exception ex) {
-//            Logger.getLogger(ReporteBean.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//            return list;
-//        } catch (SQLException ex) {
-//            Logger.getLogger(ReporteBean.class.getName()).log(Level.SEVERE, null, ex);
-//        }
     }
-
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
 }
